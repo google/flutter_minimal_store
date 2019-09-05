5e5df2390dd99fbd359f7d89181434b0ca8fb4b3
Brett Morgan <brettmorgan@google.com>
2019-09-05 14:04:44 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Flutter Store
+
+A stripped down e-commerce style app, written using the [Flutter SDK](https://flutter.dev/).
+
+This is not the proper way to lay out a Flutter project, it was built this way to enable live coding on stage. See the `typer` directory for the build steps.
+
+## Standard disclaimer
+
+This is not an officially supported Google product
---
+++ lib/styles.dart
@@ -0,0 +1,64 @@
+// Copyright 2019-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+
+abstract class Styles {
+  static const TextStyle productRowItemName = TextStyle(
+    color: Color.fromRGBO(0, 0, 0, 0.8),
+    fontSize: 18,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle productRowTotal = TextStyle(
+    color: Color.fromRGBO(0, 0, 0, 0.8),
+    fontSize: 18,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle productRowItemPrice = TextStyle(
+    color: Color(0xFF8E8E93),
+    fontSize: 13,
+    fontWeight: FontWeight.w300,
+  );
+
+  static const TextStyle searchText = TextStyle(
+    color: Color.fromRGBO(0, 0, 0, 1),
+    fontSize: 14,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle deliveryTimeLabel = TextStyle(
+    color: Color(0xFFC2C2C2),
+    fontWeight: FontWeight.w300,
+  );
+
+  static const TextStyle deliveryTime = TextStyle(
+    color: CupertinoColors.inactiveGray,
+  );
+
+  static const Color productRowDivider = Color(0xFFD9D9D9);
+
+  static const Color scaffoldBackground = Color(0xfff0f0f0);
+
+  static const Color searchBackground = Color(0xffe0e0e0);
+
+  static const Color searchCursorColor = Color.fromRGBO(0, 122, 255, 1);
+
+  static const Color searchIconColor = Color.fromRGBO(128, 128, 128, 1);
+}
